2699d6adac55944195244bf209821c93aadfc269
nvtung194208 <tung.nv194208@sis.hust.edu.vn>
2022-10-28 11:14:15 +0700

    add file and folder for screen

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+export default App =() =>{
+    return (
+        <View>
+            <Text style={{fontSize:25}}>Alo</Text>
+
+        </View>
+    )
+}
