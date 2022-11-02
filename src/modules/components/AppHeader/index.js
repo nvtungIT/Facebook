a81ffc025adebd2fc4a470f236007562051b4c9f
nvtung194208 <tung.nv194208@sis.hust.edu.vn>
2022-11-03 00:50:21 +0700

    update AppHeader

--- src/modules/components/AppHeader/index.js
+++ src/modules/components/AppHeader/index.js
@@ -1,26 +1,69 @@
 import {View, Image, Text} from 'react-native';
 import AppImage from '../../../general/constants/AppImage';
+import Icon from 'react-native-vector-icons/Fontisto';
 export default function AppHeader(props) {
   return (
     <View
       style={{
-        height: 45,
-        width: '100%',
-        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: '#ffffff',
       }}
     >
-      <Text
+      <View
         style={{
-          fontFamily: 'FACEBOLF',
-          color: '#1778F2',
-          fontWeight: '900',
-          fontSize: 25,
-          lineHeight: 45,
-          paddingLeft: 10,
+          height: 45,
         }}
       >
-        facebook
-      </Text>
+        <Text
+          style={{
+            fontFamily: 'FACEBOLF',
+            color: '#1778F2',
+            fontWeight: '900',
+            fontSize: 25,
+            lineHeight: 45,
+            paddingLeft: 10,
+          }}
+        >
+          facebook
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: '#d4d2d2',
+            width: 35,
+            height: 35,
+            borderRadius: 35,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Icon size={20} name="search" color="black" />
+        </View>
+        <View
+          style={{
+            backgroundColor: '#d4d2d2',
+            width: 35,
+            height: 35,
+            borderRadius: 35,
+
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginRight: 7.5,
+            marginLeft: 5,
+          }}
+        >
+          <Icon size={20} name="messenger" color="black" />
+        </View>
+      </View>
     </View>
   );
 }
