00f9b3a1ceead5ad6ba4b7188ea598b314af31f1
vitaemin <84129104+vitaemin@users.noreply.github.com>
2022-12-01 11:12:16 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import { StatusBar } from 'react-native'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import AppNavigator from 'modules/navigators/AppNavigator'
-import SignupScreen from 'modules/screens/SignupScreen'
-import AddPost from 'modules/views/CreatePost'
+import AppNavigator from 'modules/navigators/AppNavigator';
+import SignupScreen from 'modules/screens/SignupScreen';
+import AddPost from 'modules/views/CreatePost';
 
 function App() {
   return (
     <SafeAreaProvider>
-      {/* <StatusBar
+      <StatusBar
         animated={true}
         backgroundColor="#ffffff"
         barStyle="dark-content"
       />
-      <AppNavigator /> */}
-      <AddPost />
-	  {/* <SignupScreen/> */}
+      {/* <AppNavigator /> */}
+      {/* <AddPost /> */}
+      <SignupScreen />
     </SafeAreaProvider>
-  )
+  );
 }
-export default App
+export default App;
