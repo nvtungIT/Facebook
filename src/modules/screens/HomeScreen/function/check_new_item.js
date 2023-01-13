813872b29fd230ce979bc60ddebe16f23bbd18e0
vitaemin <84129104+vitaemin@users.noreply.github.com>
2023-01-14 01:23:54 +0700

    màn hình trang chủ & singlePost

--- src/modules/screens/HomeScreen/function/check_new_item.js
+++ src/modules/screens/HomeScreen/function/check_new_item.js
@@ -3,10 +3,9 @@ import { token, serverDomain } from './variables';
 export async function check_new_item(topId) {
   const url =
     serverDomain + 'post/check_new_item?last_id=' + topId + '&category_id=0';
-  let rs = await fetch(url, { method: 'POST' })
+  return await fetch(url, { method: 'POST' })
     .then((result) => result.json())
     .then((json) => {
       return json.data.new_items;
     });
-  return rs;
 }
