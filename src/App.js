2c14ca00adf9455d78e62fc77bd902c87434d304
vitaemin <84129104+vitaemin@users.noreply.github.com>
2023-01-23 21:12:54 +0700

    cmm

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         backgroundColor="#ffffff"
         barStyle="dark-content"
       />
-      <AppNavigator /> */}
+      <AppNavigator />
       <AddPost postData={{ isEditing: true, id: '63c5ffe8febc5c12a0b8a7f9' }} />
       {/* <SignupScreen/> */}
     </SafeAreaProvider>
